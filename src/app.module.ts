605522d698dd061c4b7f2df68f824428bf273a14
aitatsuta <171553267+aitatsuta@users.noreply.github.com>
2024-06-14 18:28:04 +0900

    created Task Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { TasksModule } from './tasks/task.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'root',
+        password: 'secret',
+        database: 'mydb-dev',
+        autoLoadEntities: true,
+        synchronize: true,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
